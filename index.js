d67255c6214654d9fc9c8e615a0db23e9829b0c8
Brian <brianmariga0@gmail.com>
2024-01-18 09:18:21 +0300

    Completed codeChallange

--- index.js
+++ index.js
@@ -7,16 +7,22 @@ document.addEventListener("DOMContentLoaded", () => {
       fetchMovieDetails(movieId);
     }
   });
+
+  document.getElementById("app").addEventListener("click", (event) => {
+    if (event.target.classList.contains("buy-ticket")) {
+      const movieId = event.target.dataset.movieId;
+      //   console.log("buyTicket");
+      buyTicket(movieId);
+    }
+  });
 });
 
 function fetchMovies() {
   fetch("http://localhost:3000/films")
     .then((response) => response.json())
     .then((data) => {
-      // Display movie list in the left menu
       displayMovieList(data);
 
-      // Display the details of the first movie
       if (data.length > 0) {
         fetchMovieDetails(data[0].id);
       }
@@ -47,4 +53,57 @@ function fetchMovieDetails(movieId) {
     .catch((error) => console.error("Error fetching movie details:", error));
 }
 
-function displayMovieDetails(movie) {}
+function displayMovieDetails(movie) {
+  const movieDetails = document.getElementById("movie-details");
+  movieDetails.innerHTML = `
+          <img src="${movie.poster}" alt="${movie.title}">
+          <h2><em>${movie.title}<em></h2>
+          <p><strong>${movie.description}</strong></p>
+          <p>Runtime: ${movie.runtime} minutes</p>
+          <p>Showtime: ${movie.showtime}</p>
+          <p>Available Tickets: ${movie.capacity - movie.tickets_sold}</p>
+          <button class="buy-ticket" id="buy-ticket" data-movie-id="${
+            movie.id
+          }">Buy Ticket</button>
+      `;
+  const button = document.querySelector("button");
+  button.addEventListener(
+    "click",
+    // console.log("buyTicket")
+    buyTicket
+  );
+}
+
+function buyTicket(movieId) {
+  fetch(`http://localhost:3000/films/${movieId}`)
+    .then((response) => response.json())
+    .then((movie) => {
+      if (movie.capacity > movie.tickets_sold) {
+        return fetch(`http://localhost:3000/films/${movieId}`, {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            tickets_sold: movie.tickets_sold + 1,
+          }),
+        });
+      } else {
+        alert(`Sorry, the showing is sold out.`);
+        throw new Error("Sold out");
+      }
+    })
+    .then((response) => response.json())
+    .then((updatedMovie) => {
+      const availableTickets = document.querySelector(
+        `[data-movie-id="${movieId}"] .available-tickets`
+      );
+      availableTickets.textContent = `Available Tickets: ${
+        updatedMovie.capacity - updatedMovie.tickets_sold
+      }`;
+      location.reload();
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
